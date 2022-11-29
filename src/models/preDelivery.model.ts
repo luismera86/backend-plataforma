1299cb1c24bee0d181a7c25a5b0138865c679e8c
Luis Mera <luismera86@gmail.com>
2022-11-28 23:01:13 -0300

    Modelo preDelivery agregado

---
+++ src/models/preDelivery.model.ts
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize'
+import db from '../db/config'
+
+// TODO ver como relacionar con las tablas del students
+
+const PreDelivery = db.define('preDelivery', {
+	preDeliveryNumber: {
+		type: DataTypes.INTEGER,
+	},
+	course: {
+		type: DataTypes.TEXT,
+	},
+	generalSlogan: {
+		type: DataTypes.TEXT,
+	},
+	sloganOne: {
+		type: DataTypes.TEXT,
+	},
+	sloganTow: {
+		type: DataTypes.TEXT,
+	},
+	sloganThree: {
+		type: DataTypes.TEXT,
+	},
+	sloganFour: {
+		type: DataTypes.TEXT,
+	},
+	sloganFive: {
+		type: DataTypes.TEXT,
+	},
+})
+
+export default PreDelivery
